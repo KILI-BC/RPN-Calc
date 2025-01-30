058896288f1a550f46b3afe6df8e7c49b19de4e0
KILI-BC <45309327+KILI-BC@users.noreply.github.com>
2025-01-30 17:16:20 +0100

    implemented basic numstack
    - added structs for stack head and elements
    - added push, pop and init functions
    - implemented basic functions

--- src/numstack.c
+++ src/numstack.c
@@ -1,1 +1,29 @@
 #include "numstack.h"
+#include <stdlib.h>
+
+numstack_result numstack_init(numstack *n)
+{
+	n->first = NULL;
+	return NUMSTACK_SUCCESS;
+}
+
+numstack_result numstack_push(numstack *n, double d)
+{
+	numstack_elem *new = malloc(sizeof(numstack_elem));
+	if(new == NULL)
+		return NUMSTACK_FALIURE;
+	new->num = d;
+	new->next = n->first;
+	n->first = new;
+	return NUMSTACK_SUCCESS;
+}
+
+double numstack_pop(numstack *n)
+{
+	double d;
+	numstack_elem *old = n->first;
+	n->first = n->first->next;
+	d = old->num;
+	free(old);
+	return d;
+}
--- src/numstack.h
+++ src/numstack.h
@@ -1,4 +1,22 @@
 #ifndef NUMSTACK_H_INCLUDED
 #define NUMSTACK_H_INCLUDED
 
+#define NUMSTACK_SUCCESS 1
+#define NUMSTACK_FALIURE 0
+
+typedef struct _numstack_elem{
+	numstack_elem * next;
+	double num;
+} numstack_elem;
+
+typedef struct _numstack{
+	numstack_elem *first;
+} numstack;
+
+typedef int numstack_result;
+
+numstack_result numstack_init(numstack *n);
+numstack_result numstack_push(numstack *n, double d);
+double numstack_pop(numstack *n);
+
 #endif
