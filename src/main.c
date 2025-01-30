f759b642006ce799d7e0471545aabe42c0c50e77
KILI-BC <45309327+KILI-BC@users.noreply.github.com>
2025-01-30 18:59:37 +0100

    added additional operations
    - added modulo operation
    - added power operation
    - added checking for division by zero

--- src/main.c
+++ src/main.c
@@ -3,8 +3,10 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define INVALID_ARGUMENT -1
+#define DIVISION_BY_ZERO -2
 
 void handle_error(numstack_result err);
 numstack_result calculate(numstack *stack_ptr, char *operation);
@@ -54,12 +56,14 @@ numstack_result calculate(numstack *stack_ptr, char *operation)
 	if(operation[1] != '\0')
 		return INVALID_ARGUMENT;
 
+
 	err = numstack_pop(stack_ptr, &arg2);
 	if(err != NUMSTACK_SUCCESS)
 		return err;
 	err = numstack_pop(stack_ptr, &arg1);
 	if(err != NUMSTACK_SUCCESS)
 		return err;
+
 
 	switch (operation[0]) {
 	case '+':
@@ -68,11 +72,23 @@ numstack_result calculate(numstack *stack_ptr, char *operation)
 	case '-':
 		res = arg1 - arg2;
 		break;
-	case '*':
+	case 'p':
 		res = arg1 * arg2;
 		break;
 	case '/':
+		if(arg2 == 0)
+			return DIVISION_BY_ZERO;
 		res = arg1 / arg2;
+		break;
+	case '%':
+		if(arg2 <= 0)
+			return INVALID_ARGUMENT;
+		while ((arg1 - arg2) > 0)
+			arg1 -= arg2;
+		res = arg1;
+		break;
+	case '^':
+		res = pow(arg1, arg2);
 		break;
 	default:
 		return INVALID_ARGUMENT;
@@ -92,6 +108,9 @@ void handle_error(numstack_result err)
 		break;
 	case NUMSTACK_STACK_EMPTY:
 		printf("The stack seems to be empty");
+		break;
+	case DIVISION_BY_ZERO:
+		printf("A dividion by zero has occured");
 		break;
 	default:
 		break;
